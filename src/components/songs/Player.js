2ddf96a046134bcaf3b98ce04486f76ea2264737
Fizikaz <mindaugojuskos@gmail.com>
2018-12-12 13:08:29 +0200

    Testing issue on Player.js loop and result checking

--- src/components/songs/Player.js
+++ src/components/songs/Player.js
@@ -19,9 +19,15 @@ class Player extends Component {
       this.waveform.pause(); 
     }
     
-
+    // if(nextProps.generateTest){
+    //   this.generateTest();
+    // }
     
   }
+
+  // generateTest() {
+  //   console.log("labas");
+  // }
 
   componentDidMount() {
     this.waveform = new WaveSurfer.create({
@@ -41,60 +47,59 @@ class Player extends Component {
     this.waveform.on('ready', () => {
 
       this.filter = this.waveform.backend.ac.createBiquadFilter();
-
-
       this.waveform.backend.setFilters([this.filter]);
 
+      this.songLength = Math.round(this.waveform.backend.getDuration());
 
-      // var duration = this.waveform.backend.getDuration();
+      this.firstRegion = Math.round(this.songLength/4);
+      this.secondRegion = Math.round(this.songLength/2);
+      this.thirdRegion = Math.round(this.songLength/(1/0.75));
 
-      var songDuration = this.waveform.backend.getDuration();
-      var songLength = Math.round(songDuration);
+      this.waveform.addRegion({
+        start: 0, // time in seconds
+        end: this.firstRegion, // time in seconds
+        color: 'rgba(0, 120, 224, 0.5)',
+        drag: false,
+        resize: false
+      });
+      this.waveform.addRegion({
+        start: this.firstRegion, // time in seconds
+        end: this.secondRegion, // time in seconds
+        color: 'rgba(107, 100, 70, 0.5)',
+        drag: false,
+        resize: false
+      });
+      this.waveform.addRegion({
+        start: this.secondRegion, // time in seconds
+        end: this.thirdRegion, // time in seconds
+        color: 'rgba(0, 120, 224, 0.5)',
+        drag: false,
+        resize: false
+      });
+      this.waveform.addRegion({
+        start: this.thirdRegion, // time in seconds
+        end: this.songLength, // time in seconds
+        color: 'rgba(107, 100, 70, 0.5)',
+        drag: false,
+        resize: false
+      });
 
-      this.firstRegion = songLength/4;
-      this.secondRegion = songLength/2;
-      this.thirdRegion = songLength/(1/0.75)
+    
 
-
-            // FIRST REGION
-            this.waveform.addRegion({
-              start: 0, // time in seconds
-              end: this.firstRegion, // time in seconds
-              color: 'rgba(0, 120, 224, 0.5)',
-              drag: false,
-              resize: false
-            });
-      
-            // SECOND REGION
-            this.waveform.addRegion({
-              start: this.firstRegion, // time in seconds
-              end: this.secondRegion, // time in seconds
-              color: 'rgba(107, 100, 70, 0.5)',
-              drag: false,
-              resize: false
-            });
-      
-            // THIRD REGION
-            this.waveform.addRegion({
-              start: this.secondRegion, // time in seconds
-              end: this.thirdRegion, // time in seconds
-              color: 'rgba(0, 120, 224, 0.5)',
-              drag: false,
-              resize: false
-            });
-      
-            // FOURTH REGION
-            this.waveform.addRegion({
-              start: this.thirdRegion, // time in seconds
-              end: songLength, // time in seconds
-              color: 'rgba(107, 100, 70, 0.5)',
-              drag: false,
-              resize: false
-            });
 
     });
 
 
+
+    function checkResults(correctAnswer, userAnswer){
+      if(correctAnswer*1.1 >= userAnswer >= correctAnswer*0.9){
+        console.log("good answer");
+        return true;
+      } else {
+        console.log("bad answer");
+        return false;
+      }
+    };
     
     this.waveform.on('audioprocess', () => {
 
@@ -103,32 +108,22 @@ class Player extends Component {
       this.filter.Q.value = 1;
       this.filter.frequency.value = this.props.filterValue;
 
-      this.currentTime = this.waveform.getCurrentTime() - 1;
+      this.currentTime = Math.round(this.waveform.getCurrentTime()) - 1;
 
-      function checkResults(correctAnswer, userAnswer){
-        if(correctAnswer*1.1 >= userAnswer >= correctAnswer*0.9){
-          console.log("good answer");
-          return true;
-        } else {
-          console.log("bad answer");
-          return false;
-        }
-
-      };
 
       //CHECK IF CURRENT TIME CORRELATES WITH THE REGION ENDING
       if(this.currentTime == this.firstRegion) {
+
         checkResults(100, 100);
       };
 
+      if(this.currentTime == 10){
+        console.log("labas");
+      }
 
     });
 
     this.waveform.load(this.props.songUrl);
-
-    
-
-
   }
 
   render() {
--- src/components/tests/EqualizerTest.js
+++ src/components/tests/EqualizerTest.js
@@ -11,17 +11,55 @@ import Slider from "../tests/Slider";
 //import QuestionCounter from "../tests/QuestionCounter";
 
 class EqualizerTest extends Component {
-  //   const rootReducer = combineReducers({
-  //         webAudioReducer
-  //     });
+  
+  // constructor(props) {
+  //   super(props);
+  //   this.player = React.createRef();
+  // }
 
   state = {
-    testTypeSelected: "fixedEqualizer",
-    currentValue: 200,
-    max: 20000,
+    testDate: "",
+    testFinished: false,
+    testId: null,
+    testScore: null,
+    testValues: [ ],
+    testSubmittedValues: [
+      // { eqValue: 450 },
+      // { eqValue: 1050 },
+      // { eqValue: 800 },
+      // { eqValue: 400 }
+    ],
+    testType: "fixedEqualizer",
+    currentValue: 16000,
+    max: 16000,
     min: 20,
-    isPlaying: false,
+    isPlaying: false
   };
+
+  componentDidMount() {
+    this.setState({
+      testDate: new Date().toLocaleString()
+    });
+
+    this.setState({
+      testValues: [
+        { eqValue: Math.floor((Math.random() * 16000) + 1)},
+        { eqValue: Math.floor((Math.random() * 16000) + 1)},
+        { eqValue: Math.floor((Math.random() * 16000) + 1)},
+        { eqValue: Math.floor((Math.random() * 16000) + 1)}
+      ]
+    });
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  finishTest = e => {
+    this.setState({
+      testFinished: true
+    });
+  }
 
   changeValue = e => {
     this.setState({
@@ -29,20 +67,39 @@ class EqualizerTest extends Component {
     });
   };
 
-  handleChange = e => {
+  handlePlaying = e => {
     this.setState({
       isPlaying: !this.state.isPlaying
     });
   };
 
+  // handleGenerateTest = e => {
+  //   this.player.current.generateTest();
+  // }
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    this.setState({
+      testDate: new Date().toLocaleString()
+    });
+
+    const newTest = this.state;
+    const { firestore, history } = this.props;
+
+    firestore.add({ collection: 'tests' }, newTest)
+    .then(() => history.push('/'));
+};
+
   render() {
     if(!this.props.song){
       return null
     }
-    // var submittedResults = [];
+
     return (
       <div>
         <Player
+          // ref={this.player}
           songUrl={this.props.song.songURL}
           testTypeSelected={this.state.testTypeSelected}
           shouldPlay={this.state.isPlaying}
@@ -57,12 +114,13 @@ class EqualizerTest extends Component {
           min={this.state.min}
         />
 
-        <button type="submit" onClick={this.handleChange} className="btn btn-success">
+        <button  onClick={this.handlePlaying} className="btn btn-success">
           Play
         </button>
         <button type="submit" className="btn btn-success">
           <Link to="/results">Finish</Link>
         </button>
+        <button onClick={this.onSubmit} className="btn btn-success" >Hello</button>
       </div>
     );
   }
@@ -82,3 +140,4 @@ export default compose(
     song: ordered.song && ordered.song[0]
   }))
 )(EqualizerTest);
+
